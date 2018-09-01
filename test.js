151c190799c01bdb21d530dc43547af8ccae1df8
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2018-09-01 14:19:45 +0100

    Add some randomness to the test
    Make sure that I always get en error in first test which tests for
    missing file. I use pseudo-random file name and check if the file does
    not exist.

--- test.js
+++ test.js
@@ -3,14 +3,23 @@
 const
   findFile = require('.')
 , assert = require('assert')
+, fs = require('fs')
 ;
 
 describe('testing findFile:', function () {
   it('find non existing file, should give error', function () {
-    findFile('.test', 'siema.txt', function (err, path) {
-      if (err) {
-        assert.ok(err instanceof Error);
-      }
+    let randomString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10); /* this is only pseudo-random but who cares? ;-) */
+    /* make sure the random file does not exist! */
+    try {
+      fs.accessSync(`${randomString}.txt`);
+      console.error(`${randomString}.txt file already exists! Exiting with error`);
+      process.exit(1); // exit with error
+    }
+    catch (err) { /* it is OK, carry on */ }
+
+    findFile('.', `${randomString}.txt`, function (err, path) {
+      assert.ok(err instanceof Error);
+      assert.equal(typeof path, 'undefined');
     });
   });
 });
