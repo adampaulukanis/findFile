326c53c032bd769b51b2d29a718fa13722ead02d
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2018-09-01 13:51:31 +0100

    First version

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 'use strict';
 
+const
+  fs = require('fs')
+;
+
 function findFile(path, searchFile, callback) {
+  fs.readdir(path, function (err, files) {
+    if (err) {
+      return callback(err);
+    }
+    files .forEach(function (file) {
+      fs.stat(path+'/'+file, function (err, stats) {
+        if (err) {
+          return callback(err);
+        }
+        if (stats.isFile() && file == searchFile) {
+          callback(undefined, path+'/'+file);
+        }
+        else if (stats.isDirectory()) {
+          findFile(path+'/'+file, searchFile, callback);
+        }
+      });
+    });
+  });
 };
 
 module.exports = findFile;
--- test.js
+++ test.js
@@ -2,4 +2,15 @@
 
 const
   findFile = require('.')
+, assert = require('assert')
 ;
+
+describe('testing findFile:', function () {
+  it('find non existing file, should give error', function () {
+    findFile('.test', 'siema.txt', function (err, path) {
+      if (err) {
+        assert.ok(err instanceof Error);
+      }
+    });
+  });
+});
